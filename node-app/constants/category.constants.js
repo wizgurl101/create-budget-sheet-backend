22f8ea674fe6cceaa2cb7b81312b5f3042bea605
Nhu Phan <nb.phan@hotmail.com>
2023-09-22 20:09:42 -0600

    move node util app to own folder

--- node-app/constants/category.constants.js
+++ node-app/constants/category.constants.js
@@ -3,13 +3,23 @@ export const CATEGORY_2 = "grocery & house supplies";
 export const CATEGORY_3 = "pet supplies";
 export const CATEGORY_4 = "misc & dining out";
 export const CATEGORIES = [CATEGORY_1, CATEGORY_2, CATEGORY_3, CATEGORY_4];
-export const CATEGORY_1_KEYWORDS = ["shell", "gas", "esso", "petrocan"];
+export const CATEGORY_1_KEYWORDS = [
+  "shell",
+  "gas",
+  "esso",
+  "petrocan",
+  "husky",
+  "petro",
+  "mobil@",
+];
 export const CATEGORY_2_KEYWORDS = [
   "safeway #",
   "superstore",
   "t&t",
   "wal-mart",
   "supercenter",
+  "save on foods #",
+  "london drugs #",
 ];
 export const CATEGORY_3_KEYWORDS = ["petland"];
 export const CATEGORY_4_KEYWORDS = [];
